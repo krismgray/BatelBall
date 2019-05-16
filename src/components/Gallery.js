c871639b4acd6554888f3f883e29dba21cc15dbc
krismgray <kris.m.gray@gmail.com>
2019-05-15 22:45:51 -0600

    added css

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavMenu from './NavMenu';
-import { Container, Header, Button, Divider, Icon, Image, Segment, Grid } from 'semantic-ui-react';
+import { Container, Header, Button, Divider, Icon, Image, Segment } from 'semantic-ui-react';
+import {Grid, Row, Col} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from 'react-image-gallery';
@@ -8,10 +9,22 @@ import YouTube from 'react-youtube';
 import '../gallery.css';
 
 class Gallery extends React.Component {
+  state = { width: window.innerWidth, visible: false }
+
+  handleResize() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  componentDidMOunt() {
+    window.addEventListener('resize', this.handleResize())
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize());
+  }
 
 
   render() {
-
     const images = [
       {
         original: require('../images/IMG1.jpeg'),
@@ -26,7 +39,9 @@ class Gallery extends React.Component {
         thumbnail: require('../images/IMG4.jpeg')
       }
     ]
-
+    const { activeItem, width, visible } = this.state;
+    const isMobile = width <= 600;
+    if (!isMobile ){
     return(
       <div>
         <div className='gal'>
@@ -42,20 +57,59 @@ class Gallery extends React.Component {
             <br />
             <div className="videosection">
               <Grid>
-                <Grid.Row columns={2}>
-                  <Grid.Column>
+                <Row>
+                  <Col xs="12" md="6">
                     <iframe width="500" height="300" src="https://www.youtube.com/embed/U9t-slLl30E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </Grid.Column>
-                  <Grid.Column>
+                  </Col>
+                  <Col xs="12" md="6">
                     <iframe width="500" height="300" src="https://www.youtube.com/embed/U9t-slLl30E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </Grid.Column>
-                </Grid.Row>
+                  </Col>
+                </Row>
               </Grid>
             </div>
+            <br />
+            <br />
           </div>
         </div>
       </div>
     )
+  }
+    else {
+      return (
+      <div>
+        <div className='gal'>
+          <div>
+            <NavMenu />
+          </div>
+          <div className='galStyle'>
+            <br />
+            <Container>
+              <ImageGallery items={images} />
+            </Container>
+            <br />
+            <br />
+            <div className="Mobilevideosection">
+              <Grid>
+                <Row>
+                  <Col xs="12">
+                    <iframe width="380" height="200" src="https://www.youtube.com/embed/U9t-slLl30E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </Col>
+                  <Col xs="12">
+                    <br />
+                    <br />
+                  </Col>
+                  <Col xs="12">
+                    <iframe width="380" height="200" src="https://www.youtube.com/embed/U9t-slLl30E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </Col>
+                </Row>
+              </Grid>
+            </div>
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+    )}
   }
 }
 
